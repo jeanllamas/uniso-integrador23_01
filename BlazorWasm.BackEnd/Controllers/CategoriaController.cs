af9a2f30d577a925b33b3896b6ac2373fa16a28c
JeanLlamas <jeanaugustollamas@outlook.com>
2023-05-25 08:29:37 -0300

    arquivos do projeto

---
+++ BlazorWasm.BackEnd/Controllers/CategoriaController.cs
@@ -0,0 +1,70 @@
+﻿using BlazorWasm.Compartilhado.Entidades;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BlazorWasmServer.Server.Controllers
+{
+    [ApiController]
+
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase 
+    {
+        private readonly ApplicationDbContext context;
+        public ClienteController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cliente>>> Get()
+        {
+            return await context.Clientes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cliente>> Get(int id)
+        {
+            var resp =await context.Clientes.FirstOrDefaultAsync(x => x.Id == id);
+            if (resp == null) { return NotFound(); }
+            return resp;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post(Cliente categoria)
+        {
+            context.Clientes.Add(categoria);
+            await context.SaveChangesAsync();
+            return categoria.Id;
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Cliente categoria)
+        {
+            context.Attach(categoria).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var categoria = await context.Clientes.FirstOrDefaultAsync(x => x.Id == id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(categoria);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+
+}
---
+++ BlazorWasm.BackEnd/Migrations/20230525102555_Clientes.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWasm.BackEnd.Migrations
+{
+    public partial class Clientes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Sobrenome = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Senha = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    Nome = table.Column<string>(type: "TEXT", nullable: true),
+                    Preco = table.Column<double>(type: "REAL", nullable: false),
+                    CategoriaId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
