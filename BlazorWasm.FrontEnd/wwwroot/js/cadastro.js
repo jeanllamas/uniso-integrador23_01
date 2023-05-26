58a847db217dcba176a1ef13669aa1fd41cbc676
JeanLlamas <jeanaugustollamas@outlook.com>
2023-05-26 05:50:29 -0300

    correções de javascript

---
+++ BlazorWasm.FrontEnd/wwwroot/js/cadastro.js
@@ -0,0 +1,17 @@
+﻿function mostrarSenha() {
+    var x = document.getElementById("senha");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function termos() {
+    const termosCheckbox = document.getElementById('termosCheck');
+    const cadastroButton = document.getElementById('cadastroBtn');
+
+    termosCheckbox.addEventListener('change', function () {
+        cadastroButton.disabled = !this.checked;
+    })
+}
